b308c77c43502995a37453c89bb2b888b712625c
ChiThien97 <71690451+ChiThien97@users.noreply.github.com>
2021-07-18 10:35:15 +0700

    Sua file AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -33,21 +33,21 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
-        $loaisp = loaiSanPham::all();
-        $nhasx = nhaSanXuat::all();
-        $nhacc = nhaCungCap::all();
-        $sanpham = sanPham::all();
-        $donhang = donDatHang::all();
-        $ctdh = chiTietDonDatHang::all();
-        $pbh = phieuBaoHanh::all();
-
-
-        View::share('loaisp', $loaisp);
-        View::share('nhasx', $nhasx);
-        View::share('nhacc', $nhacc);
-        View::share('sanpham', $sanpham);
-        View::share('donhang', $donhang);
-        View::share('ctdh', $ctdh);
-        View::share('pbh', $pbh);
+//        $loaisp = loaiSanPham::all();
+//        $nhasx = nhaSanXuat::all();
+//        $nhacc = nhaCungCap::all();
+//        $sanpham = sanPham::all();
+//        $donhang = donDatHang::all();
+//        $ctdh = chiTietDonDatHang::all();
+//        $pbh = phieuBaoHanh::all();
+//
+//
+//        View::share('loaisp', $loaisp);
+//        View::share('nhasx', $nhasx);
+//        View::share('nhacc', $nhacc);
+//        View::share('sanpham', $sanpham);
+//        View::share('donhang', $donhang);
+//        View::share('ctdh', $ctdh);
+//        View::share('pbh', $pbh);
     }
 }
